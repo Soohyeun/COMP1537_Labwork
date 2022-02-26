5db07620031d80db307016f0f4384dbcb77b8c53
Soohyeun <soohyeunp@gmail.com>
2022-02-26 15:32:43 -0800

    Add more css on lab6

--- W8_lab6/code.js
+++ W8_lab6/code.js
@@ -16,7 +16,7 @@ function calculate_subtraction(){
 
     jQuery("#result").html('Result: '+ fisrt + ' - ' + second + ' = ' + total)
 
-    jQuery("#history").append("<div class ='minus'>" + fisrt + " - " + second + " = " + total + "</div>" )
+    jQuery("#history").append("<div class ='subtract'>" + fisrt + " - " + second + " = " + total + "</div>" )
 }
 
 function calculate_multiply(){
@@ -27,25 +27,46 @@ function calculate_multiply(){
 
     jQuery("#result").html('Result: '+ fisrt + ' * ' + second + ' = ' + total)
 
-    jQuery("#history").append("<div class ='multi'>" + fisrt + " * " + second + " = " + total + "</div>" )
+    jQuery("#history").append("<div class ='multiply'>" + fisrt + " * " + second + " = " + total + "</div>" )
 }
 
 function calculate_division(){
     
     fisrt = parseInt(jQuery('#x').val())
     second = parseInt(jQuery('#y').val())
-    total = fisrt / second
+    total = Math.round(fisrt / second * 100) / 100
 
     jQuery("#result").html('Result: '+ fisrt + ' / ' + second + ' = ' + total)
 
     jQuery("#history").append("<div class ='divide'>" + fisrt + " / " + second + " = " + total + "</div>" )
 }
 
-function setup(){
-    jQuery("#addition").click(calculate_addtion)
-    jQuery("#subtraction").click(calculate_subtraction)
-    jQuery("#multiply").click(calculate_multiply)
-    jQuery("#division").click(calculate_division)
+function calculate_power(){
+    fisrt = parseInt(jQuery('#x').val())
+    second = parseInt(jQuery('#y').val())
+    total = fisrt ** second
 
+    jQuery("#result").html('Result: '+ fisrt + ' ^ ' + second + ' = ' + total)
+
+    jQuery("#history").append("<div class ='power'>" + fisrt + " ^ " + second + " = " + total + "</div>" )
+}
+
+function calculate_remainer(){
+    fisrt = parseInt(jQuery('#x').val())
+    second = parseInt(jQuery('#y').val())
+    total = fisrt % second
+
+    jQuery("#result").html('Result: '+ fisrt + ' % ' + second + ' = ' + total)
+
+    jQuery("#history").append("<div class ='remainer'>" + fisrt + " % " + second + " = " + total + "</div>" )
+}
+
+function setup(){
+    jQuery("#add_btn").click(calculate_addtion)
+    jQuery("#subtract_btn").click(calculate_subtraction)
+    jQuery("#multiply_btn").click(calculate_multiply)
+    jQuery("#divide_btn").click(calculate_division)
+    jQuery("#power_btn").click(calculate_power)
+    jQuery("#remainer_btn").click(calculate_remainer)
 }
 jQuery(document).ready(setup);
